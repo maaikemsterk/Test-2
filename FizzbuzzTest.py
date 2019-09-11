22c9db4dcd7ed43dd569a028566055f80380bced
Maaike Sterk <maaike.sterk@bpsolutions.com>
2019-09-11 11:14:01 +0200

    first try

--- FizzbuzzTest.py
+++ FizzbuzzTest.py
@@ -10,4 +10,13 @@ for num in range(1,21):
         string = string + "Buzz"
     if num % 5 != 0 and num % 3 != 0:
         string = string + str(num)
-    print(string)
+    print(string);
+
+    for num in range(1, 21):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif print(num)
